bc302ad459e50a3744c2caeaff8333b553c0b965
Júlio César <juliocesar.jcba@gmail.com>
2017-04-27 17:00:44 -0300

    conituacao

---
+++ src/main/java/com/jcba/financeiro/mb/ConsultaLancamentosBean.java
@@ -0,0 +1,34 @@
+package com.jcba.financeiro.mb;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.persistence.EntityManager;
+
+import com.jcba.financeiro.model.Lancamento;
+import com.jcba.financeiro.repository.Lancamentos;
+import com.jcba.financeiro.util.JpaUtil;
+
+@ManagedBean
+@ViewScoped
+public class ConsultaLancamentosBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private List<Lancamento> lancamentos;
+
+	public void consultar() {
+		EntityManager manager = JpaUtil.getEntityManager();
+		Lancamentos lancamentos = new Lancamentos(manager);
+		this.lancamentos = lancamentos.todos();
+
+		manager.close();
+	}
+
+	public List<Lancamento> getLancamentos() {
+		return lancamentos;
+	}
+
+}
